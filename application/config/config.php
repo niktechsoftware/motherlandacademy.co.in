51425dd379b333f5f9e4ac4c90c2914609441541
abdalftp <abdalftp@niktechsoftware>
2019-05-09 23:25:33 -0700

    add

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/motherland/';
+$config['base_url'] = 'http://localhost/motherlandacademy.co.in/';
 
 /*
 |--------------------------------------------------------------------------
